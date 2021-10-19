2626ca8b05ebcf7a42022faa423cc0b4ddebb633
ggmcjy <you@example.com>
2021-10-19 19:31:14 +0900

    2021-10-19 commit 김영한 jpa 엔티티구성

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,23 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<>();
 
 
+    // setter 사용을 왠만하면 안하고 이런식으로 변경될값을 엔티티 내에서 validation 을 해줘야 좋다.
+    //==비즈니스 로직==//
+    /*
+    재고(stock) 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /*
+    stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
